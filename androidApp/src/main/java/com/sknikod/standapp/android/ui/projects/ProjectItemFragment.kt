4c5c3c4a63a0076da9be226126d86e1931eda2e1
kolo14777 <kolo14777@gmail.com>
2022-09-26 01:48:31 +0200

    Fix attach compose view to view

--- androidApp/src/main/java/com/sknikod/standapp/android/ui/projects/ProjectItemFragment.kt
+++ androidApp/src/main/java/com/sknikod/standapp/android/ui/projects/ProjectItemFragment.kt
@@ -5,16 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.mukesh.MarkDown
 import com.sknikod.standapp.android.R
 import org.koin.android.ext.android.inject
@@ -32,8 +28,9 @@ class ProjectItemFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val markdown = ComposeView(requireContext())
+        val view =        inflater.inflate(R.layout.fragment_project_item, null, false)
 
+        val markdown = view?.findViewById<ComposeView>(R.id.web_view_project)
         lifecycleScope.launchWhenCreated {
             viewModel.getListProjects()
 
@@ -48,7 +45,7 @@ class ProjectItemFragment : Fragment() {
                                 )
                                 setContent {
                                     // In Compose world
-                                    MaterialTheme {
+                                    MdcTheme {
                                         MarkDown(
                                             text = data.value?.find {
                                                 it.id == arguments?.getInt(
@@ -66,7 +63,6 @@ class ProjectItemFragment : Fragment() {
                 }
             }
         }
-        return markdown
-
+        return view
     }
 }
